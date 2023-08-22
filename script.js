7864fb4dbb72da3b8985e293b2d042b0bccc9a04
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-08-22 14:18:13 +0200

    Aggiunta distinzione ore-minuti-secondi

--- script.js
+++ script.js
@@ -25,14 +25,29 @@ const cancella = () => {
   localStorage.removeItem("nome");
 };
 const cronometro = () => {
-  document.getElementById("tempo").innerText = time + " s";
-
+  showTime();
   x = setInterval(() => {
     orologio();
-    document.getElementById("tempo").innerText = time + " s";
+    showTime();
   }, 1000);
 };
 const orologio = () => {
   time++;
   sessionStorage.setItem("tempo", time);
+};
+
+const showTime = () => {
+  let minuti = parseInt(time / 60);
+  const ore = parseInt(minuti / 60);
+  const secondi = time % 60;
+  minuti = minuti - ore * 60;
+  console.log(ore, minuti, secondi);
+  const tempo = document.getElementById("tempo");
+  if (ore > 0) {
+    tempo.innerText = `${ore} h  ${minuti} m  ${secondi} s`;
+  } else if (minuti > 0) {
+    tempo.innerText = `${minuti} m  ${secondi} s`;
+  } else {
+    tempo.innerText = `${secondi} s`;
+  }
 };
